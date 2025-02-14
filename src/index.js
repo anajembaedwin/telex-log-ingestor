32ab18a04feac02c11430f61992aefb8ac4e01b6
anajembaedwin <anajembaedwin@gmail.com>
2025-02-14 04:01:26 +0100

    feat: added endpoint to serve the integration.json file

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.get('/health', (req, res) => {
   res.json({ status: 'OK', message: 'Log Ingestor is running' });
 });
 
-// Serve the integration.json file
+// Endpoint to serve the integration.json file
 app.get('/integration.json', (req, res) => {
     res.sendFile(path.join(__dirname, '../log-ingestor-config.json'));
   });
